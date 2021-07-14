f8748c813bdf6bb8f1f8355e73d22f599b9db4ac
jmacfarland <macfarland.jamison@gmail.com>
2021-07-14 10:45:09 -0400

    test two-user RSA encryption working

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 from btpeer import BTPeer
+from user import User
 
 def test():
     p = BTPeer(5, 10001,myid="one@one.com")
@@ -7,4 +8,22 @@ def test():
     p.begin_session_with("two")
     p.send_cipher_message("two", "This should be encrypted!")
 
-test()
+def anothertest():
+    one = User()
+    one.register("one", "one@test.com", debug=True)
+
+    two = User()
+    two.register("two", "two@test.com", debug=True)
+    pubkey_one = one.get_pubkey_pem()
+    two.add_contact("one@test.com", pubkey_one)
+
+    pubkey_two = two.get_pubkey_pem()
+    one.add_contact("two@test.com", pubkey_two)
+
+    msg_cipher = one.send("two@test.com",b'testmessage')
+    msg_plain = two.receive(msg_cipher)
+    print(msg_plain)
+
+
+
+anothertest()
--- user.py
+++ user.py
@@ -6,20 +6,40 @@ import crypt
 import getpass
 from hmac import compare_digest as compare_hash
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
-from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+#from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 
 import os
 import base64
 
 class User(object):
-    def register(self):
-        self.fullname = input("Full Name: ")
-        self.email = input("Email: ")
+    def _debug(self, message):
+        if self.debug:
+            print("%s: %s"%(self.email,message))
+
+    def register(self, fullname=None, email=None, debug=False):
+        self.debug=debug
+        if not fullname:
+            self.fullname = input("Full Name: ")
+        else:
+            self.fullname = fullname
+
+        if not email:
+            self.email = input("Email: ")
+        else:
+            self.email = email
         self.salt = os.urandom(16)
-        self.privkey = Ed25519PrivateKey.generate()
-        self.contacts = []
+        self.privkey = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+        self.contacts = {}
 
         #not the best way to do it as the key is held in memory for the duration...
         #but I don't want to have to enter password just to save changes
@@ -30,15 +50,63 @@ class User(object):
             r=8,
             p=1,
         )
-        key = kdf.derive(getpass.getpass().encode())
+        if not self.debug:
+            key = kdf.derive(getpass.getpass().encode())
+        else: #TODO: remove this after testing is done
+            key = kdf.derive("test".encode())
         self.f = Fernet(base64.urlsafe_b64encode(key))
 
-    def add_contact(self, name, email):
-        self.contacts.append({
-            "name":name,
-            "email":email
-        })
+    def add_contact(self, email, pubkey):
+        #self._debug("Adding contact %s: %s"%(email,pubkey))
+        self.contacts[email] = pubkey
 
+    def sign(self, message):
+        #returns signature of the message, not the message+signature
+        return self.privkey.sign(
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+
+    def send(self, email, message):
+        #MESSAGE must be bytes
+        data = json.dumps({
+            'message':base64.b64encode(message).decode(),
+            'signature':base64.b64encode(self.sign(message)).decode()
+        }).encode()
+
+        self._debug("Sending data %s to %s"%(data,email))
+        key = serialization.load_pem_public_key(
+            self.contacts[email],
+            backend=default_backend()
+        )
+        self._debug(key)
+        return key.encrypt(
+            b'afdsnfsdnfhdsfjsdhalfndasfdasbifasdnfuafdsnfsdnfhdsfjsdhalfndasfdasbifasdnfuafdsnfsdnfhdsfjsdhalfndasfdasbifasdnfuafdsnfsdnfhdsfjsdhalfndasfdasbifasdnfu',
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+    def receive(self, message_cipher):
+        return self.privkey.decrypt(
+            message_cipher,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+
+    ##############################################
+    #   SELF-FUNCTIONS
+    ########################
     def encrypt(self):
         data = {}
 
@@ -77,8 +145,11 @@ class User(object):
                 plain = self.decrypt(data)
                 self.fullname = plain['fullname']
                 self.email = plain['email']
-                self.privkey = Ed25519PrivateKey.from_private_bytes(
-                    base64.b64decode(plain['privkey'].encode()))
+                self.privkey = serialization.load_pem_private_key(
+                    plain['privkey'],
+                    password=None,
+                    backend=default_backend()
+                )
                 self.contacts = plain['contacts']
         except FileNotFoundError:
             self.register()
@@ -95,12 +166,14 @@ class User(object):
     def get_contacts(self):
         return self.contacts
 
-    def get_pubkey_b64(self):
-        return base64.b64encode(
-            self.privkey.public_key().public_bytes(
-                encoding=serialization.Encoding.Raw,
-                format=serialization.PublicFormat.Raw
-            ))
+    def get_pubkey(self):
+        return self.privkey.public_key()
+
+    def get_pubkey_pem(self):
+        return self.privkey.public_key().public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
 
     # UTILS
     ################################
@@ -108,12 +181,11 @@ class User(object):
         return json.dumps({
             "fullname":self.fullname,
             "email":self.email,
-            "privkey":base64.b64encode(
-                self.privkey.private_bytes(
-                    encoding=serialization.Encoding.Raw,
-                    format=serialization.PrivateFormat.Raw,
-                    encryption_algorithm=serialization.NoEncryption()
-            )).decode(),
+            "privkey":self.privkey.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption()
+            ).decode(),
             "contacts":self.contacts
         })
 
